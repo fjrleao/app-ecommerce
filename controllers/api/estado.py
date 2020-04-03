70a9445cb509c25e1429b5e6888c97ef11b8f06d
Fabio Junior <fjrleao@gmail.com>
2020-04-03 19:21:39 -0300

    Teste de funcionamento do bd atraves da tabela de estados

--- controllers/api/estado.py
+++ controllers/api/estado.py
@@ -5,4 +5,6 @@ from models.estado import Cidade, Estado
 class Estados(Resource):
     def get(self):
         estados = Estado.query.all()
+        for estado in estados:
+            print(estado.nome)
         return {'ok' : 'ok'}
