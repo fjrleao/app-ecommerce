85cec6c877adac99f5243ddd39a68166a6d0b787
Fabio Junior <fjrleao@gmail.com>
2020-04-04 09:50:38 -0300

    Funcionalidade de parser

--- controllers/api/estado.py
+++ controllers/api/estado.py
@@ -1,8 +1,22 @@
 from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.estado import Cidade, Estado
 
 class Estados(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('nome',
+                        type=str,
+                        required=True,
+                        help="Esse campo não pode ser deixado em branco."
+                        )
+
+    parser.add_argument('sigla',
+                        type=str,
+                        required=True,
+                        help="Esse campo não pode ser deixado em branco."
+                        )
+
     def get(self):
         estados = Estado.query.all()
         for estado in estados:
